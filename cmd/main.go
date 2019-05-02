6abc7a5635a07082910096d1989b462c16dc83f3
David Nix <davidnix81@gmail.com>
2019-05-01 21:01:20 -0600

    add clarifying comments

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/boltdb/bolt"
-	"short-url/db"
 	"short-url/server"
 )
 
 func main() {
-	d, err := bolt.Open("shortner.db", 0600, nil)
-	if err != nil {
-		panic(err)
-	}
-	defer d.Close()
-	server.MainDB = db.NewPersistent(d)
+	// uncomment the following lines for a persistent data store (BoltDB).
+	// Note: I'm setting a global variable which is very bad. Again, just a quick and dirty implementation.
+	// There is a known bug with the `Visits` and the persistent store. It always returns 0.
+
+	//d, err := bolt.Open("shortner.db", 0600, nil)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer d.Close()
+	//server.MainDB = db.NewPersistent(d)
 
 	fmt.Println("listening on", server.BaseURL)
 	if e := server.ListenAndServe(); e != nil {
